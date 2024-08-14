7ec2df2f5fa5f215f9aad8f2260c6817e90391b4
bishal2408 <bishallamichhane24@gmail.com>
2024-08-14 09:08:43 +0545

    middleware to handle cors policy added

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from "express"
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose"
 import bookRoute from "./routes/books.route.js"
+import cors from 'cors'
 
 const app = express()
 
-// middlewares
+// middleware for parsing request body
 app.use(express.json())
+
+// middleware for handling CORS policy
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: 'Content-Type'
+    })
+)
 
 // routes
 app.use('/books', bookRoute)
