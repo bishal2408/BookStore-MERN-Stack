511d47d7ee7da14ee53c3874e4c0200460d76189
bishal2408 <bishallamichhane24@gmail.com>
2024-08-14 10:16:10 +0545

    dev port changed to 3000, Home page added, and back button component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import DeleteBook from './pages/DeleteBook'
-import CreateBook from './pages/CreateBook'
+import CreateBooks from './pages/CreateBooks'
 import ShowBook from './pages/ShowBook'
 import EditBook from './pages/EditBook'
 
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/create' element={<CreateBooks />}/>
       <Route path='/books/details/:id' element={<ShowBook />}/>
       <Route path='/books/edit/:id' element={<EditBook />}/>
       <Route path='/books/delete/:id' element={<DeleteBook />}/>
